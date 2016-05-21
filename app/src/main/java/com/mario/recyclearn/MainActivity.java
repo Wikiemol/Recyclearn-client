25804603cf1cff6e96d22c6d2cffe9c8056020dd
Mario Massad <Mario Massad>
2016-05-20 22:19:28 -0400

    Uploading latest. Working on creating Image file

--- app/src/main/java/com/mario/recyclearn/MainActivity.java
+++ app/src/main/java/com/mario/recyclearn/MainActivity.java
@@ -1,15 +1,30 @@
 package com.mario.recyclearn;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private String imageDirectory = Environment.getExternalStorageDirectory() + "/Recyclearn";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +37,13 @@ public class MainActivity extends AppCompatActivity {
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
                 takePicture();
             }
         });
     }
 
     private void takePicture() {
-
+        dispatchTakePictureIntent();
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -53,4 +66,27 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap)extras.get("data");
+
+        }
+    }
+
+    private File createImageFile() throws IOException {
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Data());
+    }
+
 }
+
